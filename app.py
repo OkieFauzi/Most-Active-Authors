d202a7d704c74b241b0832d4328d4ee2f11a8549
Okie Fauzi Rachman <fauzirachmanokie@gmail.com>
2021-04-13 20:53:59 +0700

    Third commit

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 import requests
 import json
 
-parameter = {
-    "page": 1
-}
-
-response = requests.get('https://jsonmock.hackerrank.com/api/article_users/search', params=parameter)
-datas = response.json()['data']
+resp = requests.get('https://jsonmock.hackerrank.com/api/article_users/search')
+total_pages = resp.json()['total_pages']
 
 def active_authors(treshold):
     submission_count = []
-    for data in datas:
-        if data['submission_count'] > treshold:
-            count = data['username']
-            submission_count.append(count)
+    i = 1
+    while i <= total_pages:
+        response = requests.get('https://jsonmock.hackerrank.com/api/article_users/search?page=' + str(i))
+        datas = response.json()['data']
+        for data in datas:
+            if data['submission_count'] > treshold:
+                count = data['username']
+                submission_count.append(count)
+        i = i + 1
     return submission_count
 
 print(active_authors(10))
